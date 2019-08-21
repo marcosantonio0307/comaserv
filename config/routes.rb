f67e9921749e9533fd0200de63eafc190764206e
Marcos Antonio <marcosantoniotaina@gmail.com>
2019-08-20 22:23:42 -0300

    layout1

--- config/routes.rb
+++ config/routes.rb
@@ -4,4 +4,7 @@ Rails.application.routes.draw do
 
   get 'consulting/school' => 'consulting#school'
   get 'consulting/financial' => 'consulting#financial'
+
+  get 'contacts/doubt' => 'contacts#doubt'
+  get 'contacts/budget' => 'contacts#budget'
 end
