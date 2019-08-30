e526d9798849315b60cc2ae11ad4f3e90c582b57
Marcos Antonio <marcosantoniotaina@gmail.com>
2019-08-29 23:46:15 -0300

    more settings

---
+++ app/controllers/institutional_controller.rb
@@ -0,0 +1,2 @@
+class InstitutionalController < ApplicationController
+end
--- config/routes.rb
+++ config/routes.rb
@@ -5,7 +5,13 @@ Rails.application.routes.draw do
   get 'consulting/school' => 'consulting#school'
   get 'consulting/financial' => 'consulting#financial'
   get 'consulting/company' => 'consulting#company'
+  get 'consulting/accounting' => 'consulting#accounting'
+  get 'consulting/public' => 'consulting#public'
 
   get 'contacts/doubt' => 'contacts#doubt'
   get 'contacts/budget' => 'contacts#budget'
+  get 'contacts/mycontacts' => 'contacts#mycontacts'
+
+  get 'institutional/me' => 'institutional#me'
+  get 'institutional/equip' => 'institutional#equip'
 end
